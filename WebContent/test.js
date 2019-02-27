d23f1a09bb20651a4f91f7236a73e194ae96a6e5
dominoTao <idiotao@163.com>
2019-02-28 00:24:38 +0800

    事件调用函数

--- WebContent/test.js
+++ WebContent/test.js
@@ -3,5 +3,10 @@ window.onload=function(){
 	
 	var oExample = document.getElementById("example");
     alert(oExample);
+//    AddEvent("nations", "change", doSomething);
+    addEventListener("change", doSomething, false);
 };
+function doSomething() {
+	alert(123);
+}
 //$(document).ready();
